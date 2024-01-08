9682788b5c62b9321a71e7004926cc899e65b0f2
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-01-08 17:42:39 +0200

    added default Image in the component Profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { GlobalStyle } from "./GlobalStyle";
 import { Layout } from "./components/Layout/Layout";
 
 import { Profile } from "./components/Profile/Profile";
-import { RecipeList } from "./components/RecipeList/RecipeList";
 
 import userData from "./data/userData.json";
-import friends from "./data/friends.json";
 
 // import "./App.css";
 
@@ -14,7 +12,6 @@ function App() {
     <Layout>
       <GlobalStyle />
       <Profile user={userData} />
-      <RecipeList items={friends} />
     </Layout>
   );
 }
